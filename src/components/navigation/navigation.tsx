d2307359a31358cd40fa6d1f318d2643620a49c9
emmafattori <emmalfattori@gmail.com>
2025-02-04 21:33:07 -0500

    Updating nav styles, adding hover states and animation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -4,15 +4,11 @@ export const Navigation = () => {
     console.log('Navigation rendered');
 
     return (
-        <nav className="flex items-center justify-between w-full h-16 bg-yellow-400 px-4">
-        <div className="relative w-[8%] h-full">
-        </div>
-        <ul className="flex">
-            <li><a href="/">Home</a></li>
-            <li><a href="/recipes">Recipes</a></li>
-            <li><a href="">Community</a></li>
-            <li><a href="">Resources</a></li>
-            <li><a href="">Contact</a></li>
+        <nav className="flex items-center justify-center w-full h-16 bg-yellow-400 px-4">
+        <ul className="flex w-[30%] justify-center">
+            <li className="text-black mx-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-black after:transition-all after:duration-300 after:ease-out relative inline-block hover:after:w-full"><a href="/">Home</a></li>
+            <li className="text-black mx-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-black after:transition-all after:duration-300 after:ease-out relative inline-block hover:after:w-full"><a href="/recipes">Recipes</a></li>
+            <li className="text-black mx-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-black after:transition-all after:duration-300 after:ease-out relative inline-block hover:after:w-full"><a href="">Contact</a></li>
 
         </ul>
     </nav>
