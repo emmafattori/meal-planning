1a00f3783b20e18f9b41d3b28da2ddb36901c23e
emmafattori <emmalfattori@gmail.com>
2025-04-22 21:32:16 -0400

    Implementing shadcn components for form

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -3,6 +3,19 @@ import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useState } from "react";
 import { Button } from "@/components/ui/button"
 import { X } from "lucide-react";
+import { z } from "zod";
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 
 export interface ModalProps {
@@ -16,27 +29,46 @@ export const Modal = ( {onClose}: ModalProps ) => {
     const [instructions, setInstructions] = useState<string>('');
 
 
+    const formSchema = z.object({
+      recipeTitle: z.string().min(2).max(50),
+      recipeInstructions:z.array(z.string()),
+      recipeIngredients: z.array(z.string())
+    })
 
-const handleAddRecipe = async (e: React.FormEvent) => {
-    e.preventDefault();
-  
-    try {
-      await addDoc(collection(db, 'recipes'), {
-        title,
-        ingredients: ingredients.split(',').map(ingredient => ingredient.trim()),
-        instructions: instructions.split('.').map(instruction => instruction.trim()),
-        createdAt: serverTimestamp(),
-      });
-      alert('Recipe added successfully!');
-      setTitle('');
-      setIngredients('');
-      setInstructions('');
-      onClose();
-    } catch (error) {
-      console.error('Error adding recipe: ', error);
-      alert('Error adding recipe.');
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        recipeTitle: "",
+        recipeInstructions: [],
+        recipeIngredients: [],
+      },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+     
+      console.log(values)
     }
-  };
+    const handleAddRecipe = async (values: z.infer<typeof formSchema>) => {
+    
+      try {
+        await addDoc(collection(db, 'recipes'), {
+          title,
+          ingredients: ingredients.split(',').map(ingredient => ingredient.trim()),
+          instructions: instructions.split('.').map(instruction => instruction.trim()),
+          createdAt: serverTimestamp(),
+        });
+        alert('Recipe added successfully!');
+        setTitle('');
+        setIngredients('');
+        setInstructions('');
+        onClose();
+      } catch (error) {
+        console.error('Error adding recipe: ', error);
+        alert('Error adding recipe.');
+      }
+      };
+   
+    
     return (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
         <div className="bg-white p-8 rounded-lg shadow-lg relative">
@@ -44,7 +76,7 @@ const handleAddRecipe = async (e: React.FormEvent) => {
           <X />
         </Button>
           <h2 className="text-2xl font-bold">Add A Recipe</h2>
-          <form onSubmit={handleAddRecipe}>
+          {/* <form onSubmit={handleAddRecipe}>
           <div>
             <label htmlFor="title">Recipe Title:</label>
             <input
@@ -77,7 +109,49 @@ const handleAddRecipe = async (e: React.FormEvent) => {
           </div>
           <button type="submit">Add Recipe</button>
 
-        </form>
+        </form> */}
+            <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="recipeTitle"
+          render={({ field }) => (
+            <>
+              <FormDescription>
+                Add a new recipe to your library
+              </FormDescription>
+              <FormItem>
+              <FormLabel>Recipe Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Baked salmon" {...field} />
+              </FormControl>
+            
+              <FormMessage />
+            </FormItem>
+            <FormItem>
+              <FormLabel>Recipe Ingredients</FormLabel>
+              <FormControl>
+                <Input placeholder="1 lb of salmon" {...field} />
+              </FormControl>
+            
+              <FormMessage />
+            </FormItem>
+            <FormItem>
+              <FormLabel>Recipe Instructions</FormLabel>
+              <FormControl>
+                <Input placeholder="Preheat oven to 350 degrees F" {...field} />
+              </FormControl>
+            
+              <FormMessage />
+            </FormItem>
+            </>
+            
+      
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
       </div>
       </div>
     );
